28b815cb0cd62e04361742f76f60e091a6ea26bb
Vi <weikang0827@gmail.com>
2024-05-19 03:07:31 +0000

    ui: remove since in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,6 @@ const Footer = ({ fullWidth }) => {
   const BLOG = useConfig()
 
   const d = new Date()
-  const y = d.getFullYear()
-  const from = +BLOG.since
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
@@ -16,7 +14,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © {BLOG.author} 
           </p>
           <NotByAI />
         </div>
